0c99c9a69d50b2fb98868a922e449de79ce01adb
andrew odrain <andrewodrain@outlook.com>
2023-10-16 15:50:15 -0400

    debugging

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,3 @@
-""" Having 'test' in the prefix of the file is important. This is how pytest will know
-    it is the unit test module. Also, Flake8 is used to check for PEP8 styling guidelines compliance.
-    'flake8 --statistics """
-
 ## > Unit tests for the calculator library
 
 import calculator
